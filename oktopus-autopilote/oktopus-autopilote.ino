b7303d31d60ad7d6e7b06385a8596e10953417a6
Guillaume <guillaume.gir@gmail.com>
2016-09-14 15:14:49 -0400

    Integrated sensor data aquisition in main loop

--- oktopus-autopilote/oktopus-autopilote.ino
+++ oktopus-autopilote/oktopus-autopilote.ino
@@ -5,10 +5,44 @@
 // Revision: v0.0.1
 // Licence: Apache License Version 2.0, January 2004 http://www.apache.org/licenses/
 
-void setup() {
+#include "GPS.h"
+#include "Moisture.h"
+#include "Temperature.h"
 
+GPS gps;
+Moisture moistureSensor;
+Temperature temperatureSensor;
+
+String moisture = "Unknown";
+short temperature = 0;
+NMEAData gpsData;
+
+void setup() {
+  Serial.begin(115200);
+  gps.enable();
+  moistureSensor.enable();
+  temperatureSensor.enable();
 }
+
+uint32_t timer = millis();
 
 void loop() {
 
+  gpsData = gps.getData();
+  
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis())  timer = millis();
+
+  if (millis() - timer > 2000) {
+      
+    timer = millis(); // reset the time
+    
+    moisture = moistureSensor.getData();
+    temperature = temperatureSensor.getData();
+    
+    gps.printData(gpsData);
+    moistureSensor.printData(moisture);
+    temperatureSensor.printData(temperature);
+  }
 }
+
