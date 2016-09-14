a1bacad343faffa3ce7cc7ea504f4b182dd1100b
Guillaume <guillaume.gir@gmail.com>
2016-09-14 15:13:34 -0400

    Defined basic Sensor class requirements

---
+++ oktopus-autopilote/Sensor.h
@@ -0,0 +1,27 @@
+// Projet: Oktopus-autopilote
+// Auteur: Oktopus
+// Courriel: oktopuss7s8@outlook.com
+// Date: 04/09/2016
+// Revision: v0.0.1
+// Licence: Apache License Version 2.0, January 2004 http://www.apache.org/licenses/
+
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#define DEBUG false
+#define VERBOSE true
+
+#include <Arduino.h>
+
+//abstract class Sensor
+class Sensor {
+  
+  public:
+    virtual void disable();
+    virtual void enable();
+    virtual bool getStatus();
+    
+ protected:
+    bool _status = false;
+};
+#endif
