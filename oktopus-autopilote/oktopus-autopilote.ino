9f22ef080c2940deacdbd0155038c3fa6f5b1474
Guillaume <guillaume.gir@gmail.com>
2016-09-14 21:21:36 -0400

    Added gyroscope data acquisition to main loop

--- oktopus-autopilote/oktopus-autopilote.ino
+++ oktopus-autopilote/oktopus-autopilote.ino
@@ -6,22 +6,26 @@
 // Licence: Apache License Version 2.0, January 2004 http://www.apache.org/licenses/
 
 #include "GPS.h"
+#include "Gyroscope.h"
 #include "Moisture.h"
 #include "Temperature.h"
 
 GPS gps;
+Gyroscope gyroscope;
 Moisture moistureSensor;
 Temperature temperatureSensor;
 
+float heading = 0.00;
+NMEAData gpsData;
 String moisture = "Unknown";
 short temperature = 0;
-NMEAData gpsData;
 
 void setup() {
   Serial.begin(115200);
   gps.enable();
   moistureSensor.enable();
   temperatureSensor.enable();
+  gyroscope.enable();
 }
 
 uint32_t timer = millis();
@@ -36,11 +40,13 @@ void loop() {
   if (millis() - timer > 2000) {
       
     timer = millis(); // reset the time
-    
+
+    heading = gyroscope.getData();
     moisture = moistureSensor.getData();
     temperature = temperatureSensor.getData();
     
     gps.printData(gpsData);
+    gyroscope.printData(heading);
     moistureSensor.printData(moisture);
     temperatureSensor.printData(temperature);
   }
