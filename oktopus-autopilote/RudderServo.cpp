49dd324e4448a1e7d59147e1a9b42fdc493eb307
Guillaume <guillaume.gir@gmail.com>
2016-09-22 14:31:36 -0400

    Implemented RudderServo class.

---
+++ oktopus-autopilote/RudderServo.cpp
@@ -0,0 +1,96 @@
+// Projet: Oktopus-autopilote
+// Auteur: Oktopus
+// Courriel: oktopuss7s8@outlook.com
+// Date: 19/09/2016
+// Revision: v0.0.1
+// Licence: Apache License Version 2.0, January 2004 http://www.apache.org/licenses/
+
+#include "RudderServo.h"
+
+RudderServo::RudderServo(){
+}
+
+void RudderServo::disable(){
+
+  servo.detach();
+  if (VERBOSE) Serial.println("RudderServo disabled.");
+  _status = false;
+}
+
+void RudderServo::enable(){
+  
+  servo.attach(_digitalPin);
+  if (VERBOSE) Serial.println("RudderServo enabled.");
+  _status = true;
+}
+
+int RudderServo::getData(){
+
+  int angle = _mapRudderServoPositionToAngle(_position);
+  if (DEBUG) Serial.print("RudderServo position: ");
+  if (DEBUG) Serial.println(analogRead(_position));
+  return angle;
+}
+
+void RudderServo::printData(int angle){
+  
+  Serial.print("Rudder angle: ");
+  Serial.println(angle);
+}
+
+bool RudderServo::getStatus(){
+   
+  return _status;
+}
+
+int RudderServo::_mapRudderServoPositionToAngle(int position){
+  
+  int angle;
+  if (pos < 19){
+    angle = -(2.158 * position);
+  } else {
+    angle = -(1.464 * position);
+  }
+  return angle;
+}
+
+int RudderServo::_mapRudderServoAngleToPosition(int angle){
+ 
+  int position;
+  if (angle < 0){
+    position = -(0.4634 * angle);
+  } else {
+    position = -(0.6831 * angle);
+  }
+  return position;
+}
+
+void RudderServo::setAngle(int angle){
+  servo.write(_mapRudderServoAngleToPosition(angle));
+}
+
+void RudderServo::setPosition(int pos){
+  servo.write(pos);
+}
+
+void RudderServo::sweep(int start, int middle, int fin){
+  
+  int pos = start;
+  servo.write(pos);
+  delay(500);
+   for (pos = start; pos <= middle; pos += 1) {
+    servo.write(pos);
+    delay(15);
+    Serial.print("RudderServo position: ");
+    Serial.println(pos);
+   }
+  delay(500);
+  for (pos = middle; pos <= fin; pos += 1) {
+    servo.write(pos);
+    delay(15);
+    Serial.print("RudderServo position: ");
+    Serial.println(pos);
+  }
+  servo.write(start);
+}
+
---
+++ oktopus-autopilote/RudderServo.h
@@ -0,0 +1,34 @@
+// Projet: Oktopus-autopilote
+// Auteur: Oktopus
+// Courriel: oktopuss7s8@outlook.com
+// Date: 19/09/2016
+// Revision: v0.0.1
+// Licence: Apache License Version 2.0, January 2004 http://www.apache.org/licenses/
+
+#ifndef RUDDERSERVO_H
+#define RUDDERSERVO_H
+
+#include "Sensor.h"
+#include <Servo.h>
+
+class RudderServo : protected Sensor {
+  
+  public: 
+    RudderServo();
+    void disable();
+    void enable();
+    int getData();
+    boolean getStatus();
+    void printData(int angle);
+    void setAngle(int angle);
+    void setPosition(int pos);
+    void sweep(int start, int middle, int fin);
+    
+  private:
+    int _mapRudderServoPositionToAngle(int position);
+    int _mapRudderServoAngleToPosition(int angle);
+    Servo servo;
+    int _position;
+    int _digitalPin = 2;
+};
+#endif
