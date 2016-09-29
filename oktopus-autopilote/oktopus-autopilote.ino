a1e2d99df4f17119e1c60417f3061123479ff3d3
patrick.robitaille <patrick.robitaille@usherbrooke.ca>
2016-09-29 14:26:44 -0400

    ajout asservissement

--- oktopus-autopilote/oktopus-autopilote.ino
+++ oktopus-autopilote/oktopus-autopilote.ino
@@ -27,6 +27,14 @@ NMEAData gpsData;
 String moisture = "Unknown";
 short temperature = 0;
 int angle = 0;
+double A_teta[50];
+double A_safran[50];
+double A_act[50];
+double A_wave[50];
+double Kp =  0.218126115460278; // Valeur de Kp obtenue sur Matlab
+double Ki =  0.00121153031346156; // Valeur de Ki obtenue sur Matlab
+double Kd =  0.11698288703492; // Valeur de Kd obtenue sur Matlab
+double ts=1;
 
 void setup() {
   Serial.begin(115200);
@@ -41,7 +49,22 @@ void setup() {
   lcd.print("Oktopus");
   lcd.setCursor(0, 1);
   lcd.print("autopilot");
+
+  A_teta[0]=0;
+  A_teta[1]=0;
+  A_teta[2]=0;
   
+  A_safran[0]=0;
+  A_safran[1]=0;
+  A_safran[2]=0;
+
+  A_act[0]=0;
+  A_act[1]=0;
+  A_act[2]=0;
+
+  A_wave[0]=0;
+  A_wave[1]=0;
+  A_wave[2]=0;
 }
 
 uint32_t timer1 = millis();
@@ -113,4 +136,43 @@ int averageDataSamples(int dataSamples[], int numberOfSamples){
   average = sum/numberOfSamples;
   return average;
 }
+
+double asservissement(double Setpoint){
+int output=0;
+  A_safran[2]=Kp*A_teta[2]-Kp*A_teta[1]+Ki*ts*A_teta[1]+(Kd*A_teta[2]/ts)-((2*Kd*A_teta[1])/ts)+((Kd*A_teta[0])/ts)+A_safran[1];
+ 
+//actionneur
+
+A_act[2]=A_safran[1]+A_act[1]*0.0000457;
+
+//TheWave
+
+output=A_act[2];
+
+A_wave[2]=A_act[1]*0.6198+A_act[0]*0.425+A_wave[1]*1.3212-A_wave[0]*0.321;
+
+//décalage
+
+A_teta[0]=A_teta[1];
+A_teta[1]=A_teta[2];
+
+A_safran[0]=A_safran[1];
+A_safran[1]=A_safran[2];
+
+A_act[0]=A_act[1];
+A_act[1]=A_act[2];
+
+A_wave[0]=A_wave[1];
+A_wave[1]=A_wave[2];
+
+A_teta[2]=Setpoint-A_wave[2];
+
+if (output>41){
+  output=41;
+  }
+if (output<-41){
+  output=-41;
+  }  
+return output;
+ }
 
