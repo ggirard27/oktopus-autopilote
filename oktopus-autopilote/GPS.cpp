98db1f4159c83dc6fb2e6f8292941340bce92d01
Guillaume Roy-Girard <guillaume.roy.girard@outlook.com>
2016-10-12 12:37:37 -0400

    Changed GPS refresh rate from 1Hz to 10Hz

--- oktopus-autopilote/GPS.cpp
+++ oktopus-autopilote/GPS.cpp
@@ -28,7 +28,7 @@ void GPS::enable(){
   // the parser doesn't care about other sentences at this time
   
   // Set the update rate
-  GPSShield->sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  GPSShield->sendCommand(PMTK_SET_NMEA_UPDATE_10HZ);   // 10 Hz update rate
   // For the parsing code to work nicely and have time to sort thru the data, and
   // print it out we don't suggest using anything higher than 1 Hz
 
