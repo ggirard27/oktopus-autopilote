db7820f5894a38a8ebfb522aa4553d5e98f9a51d
Guillaume Roy-Girard <guillaume.roy.girard@outlook.com>
2016-10-25 20:34:13 -0400

    Added compile zones for boat and station code separation

--- oktopus-autopilote/oktopus-autopilote.ino
+++ oktopus-autopilote/oktopus-autopilote.ino
@@ -8,94 +8,137 @@
 #include "PIDController.h"
 #include "ESC.h"
 #include "GPS.h"
+#include "Utils.h"
 #include "Gyroscope.h"
 #include "Moisture.h"
 #include "RudderServo.h"
 #include "Temperature.h"
 #include <LiquidCrystal.h>
 
-void cycleDataOnLCD();
-int averageDataSamples(int dataSamples[], int numberOfSamples);
+/*
+ * Defines
+ */
+#define SECONDS 1000
+#define TEST_DELAY 15*SECONDS
+#define __BOAT__ 1
+#define __STATION__ 0
+#define MOTOR_SPEED 30
+/********************************************/
 
+
+/*
+ * Object instanciation
+ */
+#if __BOAT__>0
+  PIDController PIDController;
+  ESC esc(1500, 2000, 3);
+  Gyroscope gyroscope;
+  Moisture moistureSensor;
+  RudderServo rudderServo;
+  Temperature temperatureSensor;
+#endif
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-PIDController PIDController;
-ESC esc(1500, 2000, 3);
 GPS gps;
-Gyroscope gyroscope;
-Moisture moistureSensor;
-RudderServo rudderServo;
-Temperature temperatureSensor;
+/********************************************/ 
 
-double heading;
+
+/*
+ * Variable definitions and initialization
+ */
+#if __BOAT__>0
+  NMEAData nextGpsData;
+  NMEAData basecampGPSData;
+  NMEAData gpsError;
+  String moisture= "Unknown";
+  uint8_t temperature = 0;
+  double heading = 0.0;
+  uint8_t count = 0;
+  double theta = 0.0;
+  double rudder = 0.0;
+  uint32_t timer1;
+  uint32_t timer2;
+#endif /* __BOAT__ */
+
 NMEAData currentGpsData;
-NMEAData nextGpsData;
-String moisture = "Unknown";
-short temperature = 0;
-int angle = 0;
+/********************************************/
+
+
 
 void setup() {
-  Serial.begin(115200);
+
+  /*
+   * Sensor initialization
+   */
   gps.enable();
+  
+#if __BOAT__>0
+  Serial.begin(115200);
   gyroscope.enable();
   moistureSensor.enable();
   rudderServo.enable();
   temperatureSensor.enable();
+  /*  Uncomment when xbee is integrated
+  basecampGPSData = xbee.getGPSDataFromBasecamp();
+  gpsError = Utils.getGPSError(basecamp GPSData);
+  */
   lcd.begin(16, 2);
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Oktopus");
   lcd.setCursor(0, 1);
   lcd.print("autopilot");
-
-  rudderServo.setAngle(-41);
-  delay(500);
-  rudderServo.setAngle(0);
-  delay(500);
-  rudderServo.setAngle(41);
-  delay(500);
-  
+  delay(TEST_DELAY);
+  timer1 = millis();
+  timer2 = millis();
+#endif /* __BOAT__ */
+ /********************************************/
 }
 
-uint32_t timer1 = millis();
-uint32_t timer2 = millis();
-uint8_t count = 0;
-double theta = 0.0;
-double rudder = 0.0;
-
 void loop() {
-  esc.setSpeed(30, 0, 100);
+  
   currentGpsData = gps.getData();
+
+#if __BOAT__>0
+  esc.setSpeed(MOTOR_SPEED, 0, 100);
+
   nextGpsData.longitude = currentGpsData.longitude + 5;
   nextGpsData.latitude = currentGpsData.latitude + 5;
-  // if millis() or timer wraps around, we'll just reset it
+  /*  Uncomment when xbee is integrated
+  basecampGPSData = xbee.getGPSDataFromBasecamp();
+  gpsError = Utils.getGPSError(basecamp GPSData);
+  */
   if (timer1 > millis())  timer1 = millis();
   if (timer2 > millis())  timer2 = millis();
   
-  if (millis() - timer1 > 100) {
-    timer1 = millis(); // reset the time
+  if (millis() - timer1 > TS*SECONDS) {
+    
+    timer1 = millis();
     heading = gyroscope.getData(LSM_303);
     theta = PIDController.compute_theta(currentGpsData, nextGpsData, heading);
     rudder = PIDController.control_rudder(theta);
     rudderServo.setAngle(rudder);
+    
     cycleDataOnLCD();
   }
   
-  if (millis() - timer2 > 2500) {
-    gps.printData(currentGpsData);
-  }
   if (millis() - timer2 > 5000) {
-      timer2 = millis(); // reset the time
+   
+      timer2 = millis();
       moisture = moistureSensor.getData();
       temperature = temperatureSensor.getData();
       
       moistureSensor.printData(moisture);
       temperatureSensor.printData(temperature);
   }
+#endif /* __BOAT__ */
 }
 
 void cycleDataOnLCD() {
+  
   lcd.clear();
   lcd.setCursor(0, 0);
+  
+#if __BOAT__>0
   lcd.print("GYR:");
   lcd.print((int)heading);
   lcd.print(" SAF:");
@@ -103,31 +146,14 @@ void cycleDataOnLCD() {
   lcd.setCursor(0, 1);
   lcd.print("TTA:");
   lcd.print((int)theta);
-  /*
-  lcd.print(" LAT:");
-  lcd.print((int)currentGpsData.latitude);
-  */
-  /*
-  lcd.print(" LON:");
-  lcd.print((int)currentGpsData.longitude);
-  */
-  /*
   lcd.print(" HUM:");
   lcd.print(moisture);
-  */
-  /*
   lcd.print(" TMP: ");
   lcd.print(temperature);
-  */
-}
+#endif /* __BOAT__ */
 
-int averageDataSamples(int dataSamples[], int numberOfSamples){
-  int average = 0;
-  int sum = 0;
-  for (int i = 0; i < numberOfSamples; i++) {
-    sum = sum + dataSamples[i];
-  }
-  average = sum/numberOfSamples;
-  return average;
+  lcd.print(" LAT:");
+  lcd.print((int)currentGpsData.latitude);
+  lcd.print(" LON:");
+  lcd.print((int)currentGpsData.longitude);
 }
-
