21cd24741c27b4e58f71e9b22e06bd0a157b45dc
Guillaume Roy-Girard <guillaume.roy.girard@outlook.com>
2016-10-25 20:26:41 -0400

    Added functions to the Utils

--- oktopus-autopilote/Utils.h
+++ oktopus-autopilote/Utils.h
@@ -28,4 +28,38 @@ struct NMEAData {
   char lon;
   short satellites;
 };
+
+static NMEAData getGPSError(NMEAData basecampGPSData) {
+
+  NMEAData errorValue;
+  
+  const double latitudeDegreeToMeter = 11.1320, longitudeDegreeToMeter = 7.78165;
+  const double latitudeReference = 457460, longitudeReference = 726480;
+
+  double latitudeError = (latitudeReference - (basecampGPSData.latitude*10000))*latitudeDegreeToMeter;
+  double longitudeError = (longitudeReference - (basecampGPSData.longitude*10000))*longitudeDegreeToMeter;
+  errorValue.latitude = latitudeError;
+  errorValue.longitude = longitudeError;
+  return errorValue;
+}
+
+static double averageDataSamples(double dataSamples[], int numberOfSamples) {
+  double average = 0;
+  double sum = 0;
+  for (int i = 0; i < numberOfSamples; i++) {
+    sum = sum + dataSamples[i];
+  }
+  average = sum/(double)numberOfSamples;
+  return average;
+}
+
+static double averageDataSamples(int dataSamples[], int numberOfSamples) {
+  int average = 0;
+  int sum = 0;
+  for (int i = 0; i < numberOfSamples; i++) {
+    sum = sum + dataSamples[i];
+  }
+  average = sum/numberOfSamples;
+  return average;
+}
 #endif
