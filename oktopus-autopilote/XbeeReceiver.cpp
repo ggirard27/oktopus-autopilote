f281746d079ba73273ad8ff2fc56d34f3fce56bb
Guillaume Roy-Girard <guillaume.roy.girard@outlook.com>
2016-11-24 11:47:27 -0500

    Updated xbee libs gps data format.

--- oktopus-autopilote/XbeeReceiver.cpp
+++ oktopus-autopilote/XbeeReceiver.cpp
@@ -1,45 +1,75 @@
 #include "XbeeReceiver.h"
 #include "Arduino.h"
 
-XbeeReceiver::XbeeReceiver(): m_agreementX('X'), m_agreementY('Y'), m_agreementGPS('G'), m_Manuel('M'){}
+XbeeReceiver::XbeeReceiver(): m_agreementX('X'), m_agreementY('Y'), m_agreementGpsLatitudeSup('L'), m_agreementGpsLatitudeInf('I'), m_agreementGpsLongitudeSup('l'), m_agreementGpsLongitudeInf('i'), m_Manuel('M'), m_GPS('G'){}
 
 void XbeeReceiver::begin(){
 	m_servoValue = 0;
 	m_escValue = 0;
-	m_gpsValue = 0;
+	
+	m_gpsLatitude = 0.0;
+	m_gpsLongitude = 0.0;
+	m_gpsLatitudeInf = 0.0;
+	m_gpsLongitudeInf = 0.0;
+	m_gpsLatitudeIntInf = 0;
+	m_gpsLatitudeIntSup = 0;
+	m_gpsLongitudeIntInf = 0;
+	m_gpsLongitudeIntSup = 0;
+	
 	m_agreement = 'E';
 	m_mode = 'E';
 	
 	BAUDRATE = 38400;
 	
-	Serial.begin(BAUDRATE);
+	xbeeSerial.begin(BAUDRATE);
 
 }
 
 void XbeeReceiver::receiveData(){
-	if(Serial.available() > 0){
-		m_agreement = Serial.read();
+	if(xbeeSerial.available() > 0){
+		m_agreement = xbeeSerial.read();
 				
 		if(m_agreement == m_agreementX){
-			Serial.print(m_agreementX);
-			while(!Serial.available()){}
-			m_servoValue = Serial.parseInt();
+			xbeeSerial.print(m_agreementX);
+			while(!xbeeSerial.available()){}
+			m_servoValue = xbeeSerial.parseInt();
 			m_agreement = 'E';
 			m_mode = m_Manuel;
 		}			
 		else if(m_agreement == m_agreementY){
-			Serial.print(m_agreementY);
-			while(!Serial.available()){}
-			m_escValue = Serial.parseInt();
+			xbeeSerial.print(m_agreementY);
+			while(!xbeeSerial.available()){}
+			m_escValue = xbeeSerial.parseInt();
 			m_agreement = 'E';
 			m_mode = m_Manuel;
 		}
-		else if(m_agreement == m_agreementGPS){
-			Serial.print(m_agreementGPS);
-			while(!Serial.available()){}
-			m_gpsValue = Serial.parseInt();
+		else if(m_agreement == m_agreementGpsLatitudeInf){
+			xbeeSerial.print(m_agreementGpsLatitudeInf);
+			while(!xbeeSerial.available()){}
+			m_gpsLatitudeIntInf = xbeeSerial.parseInt();
 			m_agreement = 'E';
-			m_mode = m_agreementGPS;
+			m_mode = m_GPS;
+		}
+		else if(m_agreement == m_agreementGpsLatitudeSup){
+			xbeeSerial.print(m_agreementGpsLatitudeSup);
+			while(!xbeeSerial.available()){}
+			m_gpsLatitudeIntSup = xbeeSerial.parseInt();
+			m_agreement = 'E';
+			m_mode = m_GPS;
+		}
+		else if(m_agreement == m_agreementGpsLongitudeInf){
+			xbeeSerial.print(m_agreementGpsLongitudeInf);
+			while(!xbeeSerial.available()){}
+			m_gpsLongitudeIntInf = xbeeSerial.parseInt();
+			m_agreement = 'E';
+			m_mode = m_GPS;
+		}
+		else if(m_agreement == m_agreementGpsLongitudeSup){
+			xbeeSerial.print(m_agreementGpsLongitudeSup);
+			while(!xbeeSerial.available()){}
+			m_gpsLongitudeIntSup = xbeeSerial.parseInt();
+			m_agreement = 'E';
+			m_mode = m_GPS;
 		}
 	}
 }
@@ -62,16 +92,32 @@ int XbeeReceiver::getServoValue() const{
 	}
 }
 
-int XbeeReceiver::getGPSValue() const{
-	if(m_mode == m_agreementGPS){
-		return m_gpsValue;
+float XbeeReceiver::getGpsLatitude(){
+	if(m_mode == m_GPS){
+		m_gpsLatitudeInf = (float)m_gpsLatitudeIntInf;
+		m_gpsLatitudeInf = m_gpsLatitudeInf/100000;
+		m_gpsLatitude = (float)m_gpsLatitudeIntSup/10;
+		m_gpsLatitude = m_gpsLatitude+m_gpsLatitudeInf;
+		return m_gpsLatitude;
 	}
 	else{
-		return 666;
+		return 6666.6666;
+	}
+}
+
+float XbeeReceiver::getGpsLongitude(){
+	if(m_mode == m_GPS){
+		m_gpsLongitudeInf = (float)m_gpsLongitudeIntInf;
+		m_gpsLongitudeInf = m_gpsLongitudeInf/100000;
+		m_gpsLongitude = (float)m_gpsLongitudeIntSup/10;
+		m_gpsLongitude = m_gpsLongitude+m_gpsLongitudeInf;
+		return m_gpsLongitude;
+	}
+	else{
+		return 6666.6666;
 	}
 }
 
 char XbeeReceiver::getMode() const{
 	return m_mode;
 }
-
--- oktopus-autopilote/XbeeReceiver.h
+++ oktopus-autopilote/XbeeReceiver.h
@@ -2,32 +2,46 @@
 #define XbeeReceiver_h
 
 #include "Arduino.h"
+#define xbeeSerial Serial3
 
 class XbeeReceiver{
 	
 	public:
 	
 	XbeeReceiver();	
-	void begin();	//Initialise la communication sÃ©rie et initialise les attributs de la class.
-	void receiveData();	//RÃ©cupÃ¨re les valeurs du nunchuk -> m_servoValue, m_escValue, m_gpsValue.
+	void begin();	//Initialise la communication série et initialise les attributs de la class.
+	void receiveData();	//Récupère les valeurs du nunchuk -> m_servoValue, m_escValue, m_gpsValue.
 	int getEscValue() const;	//Retourne la valeur m_escValue pour la drive seulement si le mode == Manuel
 	int getServoValue() const;	//Retourne la valeur m_servoValue pour le servo seulement si le mode == Manuel
-	int getGPSValue() const;	//Retourne la position m_gspValue de la station fixe seulement si le mode == GPS
-	char getMode() const;	//Retourne le mode de contrÃ´le m_mode. Manuel: (M) ou GPS: (G)
+	float getGpsLatitude();	//Retourne la latitude seulement si le mode == GPS
+	float getGpsLongitude();	//Retourne la longitude seulement si le mode == GPS
+	char getMode() const;	//Retourne le mode de contrôle m_mode. Manuel: (M) ou GPS: (G)
 	private:
 	
 	int BAUDRATE;	//38400
 	int m_servoValue;	//Valeur du servo
 	int m_escValue;		//Valeur du ESC
-	int m_gpsValue;		//Valeur GPS Station fixe
+	
+	float m_gpsLatitude;	//Valeur Latitude	
+	float m_gpsLongitude;	//Valeur Longitude
+	float m_gpsLatitudeInf;
+	float m_gpsLongitudeInf;
+	int m_gpsLatitudeIntInf;
+	int m_gpsLatitudeIntSup;
+	int m_gpsLongitudeIntInf;
+	int m_gpsLongitudeIntSup;
 	
 	const char m_agreementX;	
 	const char m_agreementY;
-	const char m_agreementGPS;
+	const char m_agreementGpsLatitudeInf;
+	const char m_agreementGpsLatitudeSup;
+	const char m_agreementGpsLongitudeInf;
+	const char m_agreementGpsLongitudeSup;
 	const char m_Manuel;
+	const char m_GPS;
+	
 	char m_agreement;	
 	char m_mode; 
 };
 
 #endif
-
